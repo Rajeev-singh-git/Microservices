15d30cfe5747c0543a8d191b553408ef30e33d1d
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-12-29 14:21:39 +0530

    Spring Boot app Hotel Service conncetd with PostgreSQL DB

---
+++ HotelService/src/main/java/com/lcwd/HotelService/services/HotelService.java
@@ -0,0 +1,14 @@
+package com.lcwd.HotelService.services;
+
+import com.lcwd.HotelService.entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel create (Hotel hotel);
+
+    List<Hotel> getAll();
+
+    Hotel get(String id);
+}
