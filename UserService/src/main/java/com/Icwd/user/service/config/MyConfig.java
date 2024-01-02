bcd12b16240ec1c5c3c453c6ad4ab0a984b6c128
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-01-02 09:58:43 +0530

    Using Feign Client

--- UserService/src/main/java/com/Icwd/user/service/config/MyConfig.java
+++ UserService/src/main/java/com/Icwd/user/service/config/MyConfig.java
@@ -1,9 +1,10 @@
 package com.Icwd.user.service.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class MyConfig {
--- UserService/src/main/java/com/Icwd/user/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/Icwd/user/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.Icwd.user.service.entities.Hotel;
 import com.Icwd.user.service.entities.Rating;
 import com.Icwd.user.service.entities.User;
 import com.Icwd.user.service.exceptions.ResourceNotFoundException;
+import com.Icwd.user.service.external.services.HotelService;
 import com.Icwd.user.service.repositories.UserRepository;
 import com.Icwd.user.service.services.UserService;
 import org.slf4j.Logger;
@@ -28,6 +29,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private RestClient client;
+
+    @Autowired
+    private HotelService hotelService;
 
 
     private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
@@ -56,6 +60,7 @@ public class UserServiceImpl implements UserService {
         // fetch Rating of the above user from Rating Service
 
         ParameterizedTypeReference<List<Rating>> typeRef = new ParameterizedTypeReference<List<Rating>>() {};
+     //   List<Rating> ratingsOfUser = client.get().uri("http://RATING-SERVICE/ratings/user/" +user.getUserId())
         List<Rating> ratingsOfUser = client.get().uri("http://localhost:8083/ratings/user/" +user.getUserId())
                 .retrieve()
                 .body(typeRef);
@@ -64,12 +69,13 @@ public class UserServiceImpl implements UserService {
 
          List<Rating> ratingList = ratingsOfUser.stream().map(rating -> {
              //api call to get hotel service
-             Hotel hotels =  client.get().uri("http://localhost:8081/hotels/"+rating.getHotelId())
+             //Hotel hotels =  client.get().uri("http://HOTEL-SERVICE/hotels/"+rating.getHotelId())
+             /* Hotel hotel =  client.get().uri("http://localhost:8081/hotels/"+rating.getHotelId())
                .retrieve()
-                .body(new ParameterizedTypeReference<Hotel>() {});
-
+               .body(new ParameterizedTypeReference<Hotel>() {});  */
+             Hotel hotel = hotelService.getHotel(rating.getHotelId());
           //set the hotel to the rating
-           rating.setHotel(hotels);
+           rating.setHotel(hotel);
           // return the rating
            return rating;
 
