27d9e7d013158d17d255c77c397b22be5ec8382f
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-01-03 23:20:03 +0530

    Feign Client Post Call

--- UserService/src/test/java/com/Icwd/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/Icwd/user/service/UserServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.Icwd.user.service;
 
+import com.Icwd.user.service.entities.Rating;
+import com.Icwd.user.service.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@Autowired
+	private RatingService ratingService;
+
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+	void createRating(){
+		Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("This is created using feign client").build();
+		Rating savedRating = ratingService.createRating(rating);
+		System.out.println("New rating created");
+	}
+
+
 
 }
