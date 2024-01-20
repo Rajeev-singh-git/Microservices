e9a548686b135290259527709df58ed9a2df4977
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-01-20 23:50:47 +0530

    Added builder annotation in User Entity

--- UserService/src/main/java/com/Icwd/user/service/entities/User.java
+++ UserService/src/main/java/com/Icwd/user/service/entities/User.java
@@ -1,10 +1,7 @@
 package com.Icwd.user.service.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name="micro_user")
 public class User {
     @Id
